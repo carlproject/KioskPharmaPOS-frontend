8ca0d27e0207e263972093b9df8b93963e453e14
mtthw <matbalinton@gmail.com>
2024-10-19 14:51:29 +0800

    Add Firebase Console Auth in register

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function AppRoutes() {
   return (
     <Router>
       <Routes>
       <Route path='/login' Component={Login}/>
+      <Route path='/register' Component={Register}/>
       </Routes>
     </Router>
   )
