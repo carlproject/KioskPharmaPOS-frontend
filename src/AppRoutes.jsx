f67871331f18e6eb21b49345ad2b092d0df2dded
mtthw <matbalinton@gmail.com>
2024-10-19 21:13:46 +0800

    config analytics

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 import Login from './pages/Login';
 import Register from './pages/Register';
+import ForgetPassword from './pages/ForgetPassword';
 
 function AppRoutes() {
   return (
@@ -9,6 +10,7 @@ function AppRoutes() {
       <Routes>
       <Route path='/login' Component={Login}/>
       <Route path='/register' Component={Register}/>
+      <Route path='/forgot-password' Component={ForgetPassword}/>
       </Routes>
     </Router>
   )
