44bd3b6fe976dab53489c4bb644c2e907502fc2e
mtthw <matbalinton@gmail.com>
2024-10-20 21:44:53 +0800

    Done with layout compo

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,15 +4,17 @@ import Carousell from '../components/LandingPage/Carousell'
 import Hero from '../components/LandingPage/Hero'
 import Feature from '../components/LandingPage/Feature'
 import Nav from '../components/Nav'
+import Footer from '../components/Footer'
 
 function LandingPage() {
   return (
-    <div className='w-full'>
+    <div className='w-full overflow-hidden overflow-x-hidden'>
     <Nav />
     <Hero />
     <Heading />
     <Carousell />
     <Feature />
+    <Footer />
     </div>
   )
 }
