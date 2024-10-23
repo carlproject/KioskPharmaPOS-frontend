d7c42cc926284cbc786ed63344750babe0334202
mtthw <matbalinton@gmail.com>
2024-10-23 17:05:48 +0800

    Re-update landing page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,9 @@ function Footer() {
   <div className="mx-auto w-full overflow-hidden max-w-screen-xl p-4 py-6 lg:py-8">
       <div className="md:flex md:justify-between">
         <div className="mb-6 md:mb-0">
-            <a href="https://flowbite.com/" className="flex items-center">
+            <a href="#" className="flex items-center">
                 <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 me-3" alt="FlowBite Logo" />
-                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Chechacio</span>
             </a>
         </div>
         <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
