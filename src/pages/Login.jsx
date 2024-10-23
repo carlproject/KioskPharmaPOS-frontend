37e8c235c93fe22f9d0f3d82d260363923345bba
mtthw <matbalinton@gmail.com>
2024-10-23 16:44:25 +0800

    Add hashed in password

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,29 +1,60 @@
-import React from 'react'
-import { app, analytics }from '../config/firebase'
+import React, { useState } from 'react';
+import { app, analytics, db } from '../config/firebase';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { collection, query, where, getDocs } from 'firebase/firestore';
+import bcrypt from 'bcryptjs';
 import LoginComponent from '../components/authComponents/LoginComponent';
 
-const provider =  new GoogleAuthProvider();
+const provider = new GoogleAuthProvider();
 const auth = getAuth(app);
 
 function Login() {
+  const [loginError, setLoginError] = useState('');
 
-  const signInWithGoogle = async(e) => {
+  const signInWithGoogle = async (e) => {
     e.preventDefault();
     try {
       const result = await signInWithPopup(auth, provider);
-
       console.log(result.user);
-    }
-    catch(error) {
+    } catch (error) {
       console.log('Something went wrong', error);
     }
-  }
+  };
 
-    return (
-      <LoginComponent signInWithGoogle={signInWithGoogle} />
-    );
-  }
+  const signInWithUsernameAndPassword = async (username, password) => {
+    try {
+      const usersCollection = collection(db, 'users');
+      const q = query(usersCollection, where('username', '==', username));
+      const querySnapshot = await getDocs(q);
 
-export default Login
+      if (querySnapshot.empty) {
+        setLoginError('User not found');
+        return;
+      }
 
+      const userData = querySnapshot.docs[0].data();
+      const hashedPassword = userData.password;
+
+      const isPasswordCorrect = await bcrypt.compare(password, hashedPassword);
+
+      if (isPasswordCorrect) {
+        console.log('Login successful!');
+      } else {
+        setLoginError('Incorrect password');
+      }
+    } catch (error) {
+      console.error('Error logging in: ', error);
+      setLoginError('Something went wrong');
+    }
+  };
+
+  return (
+    <LoginComponent
+      signInWithGoogle={signInWithGoogle}
+      signInWithUsernameAndPassword={signInWithUsernameAndPassword}
+      loginError={loginError}
+    />
+  );
+}
+
+export default Login;
