b2103d9246f81c5f76fdda13c656bf5b5c333838
mtthw <matbalinton@gmail.com>
2024-10-26 22:01:02 +0800

    Use outlet

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 
 import Login from './pages/Login';
-import ForgetPassword from './pages/ForgetPassword';
 import LandingPage from './pages/LandingPage';
 import Cart from './components/pos/Cart';
 import RegisterPage from './pages/RegisterPage';
 import MeetTheTeam from './components/MeetTheTeam';
-import Nav from './components/Nav';
 import NavLayout from './layout/NavLayout';
+import Contact from './components/Contact';
+import ForgotPasswordComponent from './components/authComponents/ForgotPasswordComponent';
 
 function AppRoutes() {
 
@@ -16,11 +16,12 @@ function AppRoutes() {
       <Routes>
       <Route path='/login' Component={Login}/>
       <Route path='/register' Component={RegisterPage}/>
-      <Route path='/forgot-password' Component={ForgetPassword}/>
+      <Route path='/forgot-password' Component={ForgotPasswordComponent}/>
       
       <Route element={<NavLayout />}>
           <Route path="/" Component={LandingPage} />
           <Route path="/meet-the-team" Component={MeetTheTeam} />
+          <Route path="/contact-us" Component={Contact} />
         </Route>
       
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -98,7 +98,7 @@ function Nav() {
       </div>
     </div>
 
-    <div id="mega-menu-full-dropdown" class="mt-1 hidden absolute left-[30%] z-20 rounded-md py-12 px-4 border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600">
+    <div id="mega-menu-full-dropdown" class="mt-1 hidden absolute left-[23%] z-20 rounded-md py-12 px-4 border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600">
         <div class="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
             <ul>
                 <li>
