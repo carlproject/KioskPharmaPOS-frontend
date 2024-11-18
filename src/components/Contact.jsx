12cbfbde273d838c43c01581e739516cf0678d02
mtthw <matbalinton@gmail.com>
2024-11-18 18:30:34 +0800

    Add contact collections

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,41 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { collection, addDoc } from 'firebase/firestore';
+import {ToastContainer, toast } from 'react-toastify';
+import { db } from '../config/firebase';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function Contact() {
+  const [formData, setFormData] = useState({
+    email: '',
+    subject: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({ ...formData, [id]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const contactCollection = collection(db, 'contact');
+      await addDoc(contactCollection, formData);
+      toast.success('Message sent successfully!', {
+        position: 'top-right',
+        autoClose: 3000,
+      });
+      setFormData({ email: '', subject: '', message: '' }); // Reset the form
+    } catch (error) {
+      toast.error('Failed to send message. Please try again later.', {
+        position: 'top-right',
+        autoClose: 3000,
+      });
+      console.error('Error saving message to Firestore:', error);
+    }
+  };
+
   return (
     <section className="bg-white dark:bg-gray-900">
+    <ToastContainer />
       <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
+        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+          Contact Us
+        </h2>
         <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
           Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.
         </p>
-        <form action="#" className="space-y-8">
+        <form onSubmit={handleSubmit} className="space-y-8">
           <div>
-            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+              Your email
+            </label>
             <input
               type="email"
               id="email"
               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
               placeholder="matbalinton@gmail.com"
+              value={formData.email}
+              onChange={handleChange}
               required
             />
           </div>
           <div>
-            <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
+            <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+              Subject
+            </label>
             <input
               type="text"
               id="subject"
               className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
               placeholder="Let us know how we can help you"
+              value={formData.subject}
+              onChange={handleChange}
               required
             />
           </div>
           <div className="sm:col-span-2">
-            <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+            <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">
+              Your message
+            </label>
             <textarea
               id="message"
               rows="6"
               className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
               placeholder="Leave a comment..."
+              value={formData.message}
+              onChange={handleChange}
+              required
             ></textarea>
           </div>
           <button
